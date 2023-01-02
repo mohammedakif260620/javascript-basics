8ea33be9f874e4cd89e3d7b9a7da69b2d6b30528
Mohammed-Akif <117740605+mohammedakif260620@users.noreply.github.com>
2023-01-02 20:32:05 +0530

    Update strings.js

--- basics/strings.js
+++ basics/strings.js
@@ -1,3 +1,23 @@
 "Hello World"
 ‘Hello World’
 `Hello World`
+' 5 plus 5 is ${5+5}'  wrong
+"5 plus 5 is ${5+5}"  wrong
+BACKTICS:> 
+  `5 plus 5 is ${5+5}`  (TRUE WAY )
+  OUTPUT= '5 plus 5 is 10'
+
+writting multiple lines : with back tics(shift enter to shift for next line)
+`
+name: akif
+city:guntur
+designation:ui -dev
+`
+escape character with new line
+[JSBIN.COM]
+I\P : 
+"Nmae :Akif\ncity:guntur"
+o\p:
+"Name:Akif
+city:guntur"
+
