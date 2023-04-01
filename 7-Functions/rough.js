4c55366886bcb7fbc87258a2bb1e75370ff9b3f2
mohammedakif260620 <mohammedakif260620@gmail.com>
2023-04-01 13:07:37 +0530

    functions added

---
+++ 7-Functions/rough.js
@@ -0,0 +1,17 @@
+function mathOperations(a, b) { // Here a , b are two parameters
+    console.log(`value of a = ${a}`);
+    console.log(`value of b = ${b}`);
+    const addition = a + b;
+    const subtraction = a - b;
+    const multiply = a * b;
+    const division = a / b;
+    const modulo = a % b;
+  
+    console.log(addition);
+    console.log(subtraction);
+    console.log(multiply);
+    console.log(division);
+    console.log(modulo);
+  }
+
+  mathOperations(9,8);
